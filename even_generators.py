b787d69ff69362466b1be5890336f10851b615f5
QuameOphory <quameophory@yahoo.com>
2022-02-16 08:26:22 +0000

    added a fibonacci generator function

--- even_generators.py
+++ even_generators.py
@@ -1,12 +1,26 @@
+from itertools import count
+from re import A
+
+
 def even_generator(n):
     if n == 0: yield 0
     if n < 0: raise TypeError('can"t generate negative even numbers')
     for i in range(n):
         if i % 2 == 0: yield i
 
+def fibonacci_generator(n):
+    a, b, count = 0, 1, 1
+    while count <= n:
+        yield a
+        a, b = b, a+b
+        count += 1
+
+
 def main():
     for i in even_generator(-3):
         print(i, end=' ')
 
 if __name__ == '__main__':
-    main()
+    # main()
+    for i in fibonacci_generator(10):
+        print(i)
