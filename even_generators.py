68b8c8a9903c15d60559e45d7b52776ac28495e7
QuameOphory <quameophory@yahoo.com>
2022-02-16 07:41:13 +0000

    catching negative arguments

--- even_generators.py
+++ even_generators.py
@@ -1,10 +1,11 @@
 def even_generator(n):
     if n == 0: yield 0
+    if n < 0: raise TypeError('can"t generate negative even numbers')
     for i in range(n):
         if i % 2 == 0: yield i
 
 def main():
-    for i in even_generator(0):
+    for i in even_generator(-3):
         print(i, end=' ')
 
 if __name__ == '__main__':
